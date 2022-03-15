4ffefcfa14f8fc944ce7f5b552f1892ac6f437ee
IsmaeelAA849 <82838184+IsmaeelAA849@users.noreply.github.com>
2022-03-15 09:43:09 -0400

    Delete PastPages directory

--- PastPages/nytimes.py
+++
@@ -1,9 +0,0 @@
-import re
-import sys
-
-f1 = open("itemlist.txt","r")
-
-for line in f1:
-    if (re.search(r'new-york-times',line)):
-        print(line,end='')
-
--- PastPages/russia.py
+++
@@ -1,8 +0,0 @@
-import re
-import sys
-f1 = open("itemlist.txt","r")
-for line in f1:
-    if (re.search(r'russia-today',line)):
-        print(line,end='')
-
-
